39d534f14e9f277205428cc91b7538d4b5bc8329
yuchenxue <yuchenxue@cute.neko>
2025-09-20 17:32:17 +0800

    Bump version

--- src/main/kotlin/cute/neko/kawakaze/events/MinecraftInitializeEvent.kt
+++ src/main/kotlin/cute/neko/kawakaze/events/MinecraftInitializeEvent.kt
@@ -1,8 +1,5 @@
 package cute.neko.kawakaze.events
 
 import cute.neko.event.Event
-import net.minecraft.ItemStack
-import net.minecraft.ShapedRecipes
-import net.minecraft.ShapelessRecipes
 
 object MinecraftInitializeEvent : Event
