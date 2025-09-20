78c0d37c63f9ba0fd9ffe901c5984948f126b17d
yuchenxue <yuchenxue@cute.neko>
2025-09-20 18:15:17 +0800

    又忘记了

--- src/main/kotlin/cute/neko/kawakaze/EventHandler.kt
+++ src/main/kotlin/cute/neko/kawakaze/EventHandler.kt
@@ -1,6 +1,8 @@
 package cute.neko.kawakaze
 
 import com.google.common.eventbus.Subscribe
+import cute.neko.kawakaze.prepare.Prepares
+import cute.neko.kawakaze.prepare.types.RecipePreparable
 import cute.neko.kawakaze.registry.recipe.RecipeDelegate
 import cute.neko.kawakaze.service.Service
 import net.xiaoyu233.fml.reload.event.MITEEvents
@@ -12,6 +14,7 @@ object EventHandler : Service {
     @Suppress("unused")
     @Subscribe
     private fun onRecipeRegister(event: RecipeRegistryEvent) {
+        Prepares.RECIPE_PREPARES.forEach(RecipePreparable::prepare)
         RecipeDelegate.register(event)
     }
 
