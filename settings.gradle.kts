41b5cd39c5f1886e482cc7683233d2d66f8d05cc
yuchenxue <1816271774@qq.com>
2026-03-15 20:08:12 +0800

    Update Project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,10 @@
 pluginManagement {
     repositories {
+        mavenLocal()
         gradlePluginPortal()
         mavenCentral()
-
-        maven {
-            name = "Fabric"
-            url = uri("https://maven.fabricmc.net/")
-        }
-
-        maven {
-            url = uri("https://gitlab.com/mite-mod/repo/raw/main/maven")
-        }
-    }
-
-    plugins {
-        val kotlin_version: String by settings
-        val loom_version: String by settings
-
-        kotlin("jvm") version kotlin_version
-        id("fml-loom") version loom_version
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.limingzxc.top/repository/maven-public/")
     }
 }
 
